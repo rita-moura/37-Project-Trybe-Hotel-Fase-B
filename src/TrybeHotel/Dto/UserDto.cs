0fb8af77efd7e40feeaf41a79a8ef8010824b2f8
Rita Moura <ritamoura520@gmail.com>
2023-09-19 14:15:42 -0300

    Fix: corrige LoginDto.

--- src/TrybeHotel/Dto/UserDto.cs
+++ src/TrybeHotel/Dto/UserDto.cs
@@ -15,7 +15,7 @@ namespace TrybeHotel.Dto
 
     public class LoginDto {
       
-        public string Email {get; set;}
-        public string Password {get; set;}
+        public string? Email {get; set;}
+        public string? Password {get; set;}
     }
 }
