9d31e2fbbf9b68756c137b0c42cad9a5f43b50e7
Rita Moura <ritamoura520@gmail.com>
2023-09-19 14:19:14 -0300

    Feat: adiciona métodos Generate e AddClaims.

--- src/TrybeHotel/Services/TokenGenerator.cs
+++ src/TrybeHotel/Services/TokenGenerator.cs
@@ -1,8 +1,8 @@
 using System.IdentityModel.Tokens.Jwt;
+using Microsoft.IdentityModel.Tokens;
 using System.Security.Claims;
 using System.Text;
 using Microsoft.Extensions.Options;
-using Microsoft.IdentityModel.Tokens;
 using TrybeHotel.Models;
 using TrybeHotel.Dto;
 
@@ -21,12 +21,38 @@ namespace TrybeHotel.Services
         }
         public string Generate(UserDto user)
         {
-            throw new NotImplementedException();
+            var tokenHandler = new JwtSecurityTokenHandler();
+            var encode = Encoding.ASCII.GetBytes(_tokenOptions.Secret);
+            var claims = AddClaims(user);
+
+            var tokenDescriptor = new SecurityTokenDescriptor
+            {
+                Subject = new ClaimsIdentity(claims),
+                Expires = DateTime.UtcNow.AddDays(_tokenOptions.ExpiresDay),
+                SigningCredentials = new SigningCredentials
+                (
+                    new SymmetricSecurityKey(encode),
+                    SecurityAlgorithms.HmacSha256Signature
+                )
+            };
+
+
+            var token = tokenHandler.CreateToken(tokenDescriptor);
+            return tokenHandler.WriteToken(token);
         }
 
         private ClaimsIdentity AddClaims(UserDto user)
         {
-            throw new NotImplementedException();
+            var claims = new ClaimsIdentity();
+
+            claims.AddClaim(new Claim(ClaimTypes.Email, user.Email!));
+
+            if (user.UserType == "admin")
+            {
+                claims.AddClaim(new Claim(ClaimTypes.Role, "admin"));
+            }
+
+            return claims;
         }
     }
 }
