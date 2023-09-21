18adbf7f3d5ab1b5beadda88b9f65a33f68c5358
Rita Moura <ritamoura520@gmail.com>
2023-09-21 15:38:16 -0300

    Feat: adiciona o método Add do BookingRepository

--- src/TrybeHotel/Repository/BookingRepository.cs
+++ src/TrybeHotel/Repository/BookingRepository.cs
@@ -13,7 +13,54 @@ namespace TrybeHotel.Repository
 
         public BookingResponse Add(BookingDtoInsert booking, string email)
         {
-            throw new NotImplementedException();
+            var roomToBook = _context.Rooms.Find(booking.RoomId);
+
+            var user = _context.Users.Where(u => u.Email == email).FirstOrDefault();
+
+            if (roomToBook.Capacity >= booking.GuestQuant)
+            {
+                var bookingToSave = new Booking
+                {
+                    CheckIn = booking.CheckIn,
+                    CheckOut = booking.CheckOut,
+                    GuestQuant = booking.GuestQuant,
+                    RoomId = booking.RoomId,
+                    UserId = user.UserId
+                };
+
+                _context.Bookings.Add(bookingToSave);
+                _context.SaveChanges();
+
+                var response = from book in _context.Bookings
+                              orderby book.BookingId
+                              select new BookingResponse
+                              {
+                                  BookingId = book.BookingId,
+                                  CheckIn = book.CheckIn,
+                                  CheckOut = book.CheckOut,
+                                  GuestQuant = book.GuestQuant,
+                                  Room = new RoomDto
+                                  {
+                                      RoomId = book.Room.RoomId,
+                                      Name = book.Room.Name,
+                                      Capacity = book.Room.Capacity,
+                                      Image = book.Room.Image,
+                                      Hotel = new HotelDto
+                                      {
+                                          HotelId = book.Room.Hotel.HotelId,
+                                          Name = book.Room.Hotel.Name,
+                                          Address = book.Room.Hotel.Address,
+                                          CityId = book.Room.Hotel.CityId,
+                                          CityName = book.Room.Hotel.City.Name
+                                      }
+                                  }
+                              };
+                return response.Last();
+            }
+            else
+            {
+                return null;
+            }
         }
 
         public BookingResponse GetBooking(int bookingId, string email)
